37e6e05113c7d534876dc15fa25db55d606dfab1
chengkarmen <karmencheng6@gmail.com>
2025-12-31 13:05:09 +0800

    lab3

--- labs/lab03/exercise1/exercise1.py
+++ labs/lab03/exercise1/exercise1.py
@@ -1,2 +1,10 @@
 def count_bright_spots(pixels):
-    pass
+    count = 0
+    for i in range(len(pixels)):
+        if i == 0 or i == len(pixels) - 1 or len(pixels) == 2 or len(pixels) == 1:
+            continue
+            
+        if pixels[i] > (pixels[i - 1]) and pixels[i]  > (pixels[i + 1]):
+            count += 1
+    return count
+
--- labs/lab03/exercise2/exercise2.py
+++ labs/lab03/exercise2/exercise2.py
@@ -1,5 +1,16 @@
 def find_station(stations, name):
-    pass
+    if name not in stations:
+        return 
+    for i in range(len(stations)):
+        if stations[i] == name:
+            return i
+
+        
+
 
 def count_stops(stations, start, stop):
-    pass
+    start = find_station(stations, start)
+    stop = find_station(stations, stop)
+    if start == None or stop == None:
+        return -1
+    return abs(stop - start)
